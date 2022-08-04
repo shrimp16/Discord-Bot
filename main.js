8305fe768adf31ccf2751265062b9cefd004df57
Shrimp <lcamara0916@gmail.com>
2022-08-04 17:40:41 +0000

    The bot can now answer to a simple /ping

--- main.js
+++ main.js
@@ -7,7 +7,19 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
 client.on('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+
+    if (!interaction.isChatInputCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+
 });
 
 // Login to Discord with your client's token
