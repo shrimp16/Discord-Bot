8f014246afc15b4006a6faae3b515d6944fb0fae
Shrimp <lcamara0916@gmail.com>
2022-08-05 20:05:57 +0000

    Started trying out how to organize the commands in a better way

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, ClientUser } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const commandsPath = path.join(__dirname, 'commands');
+
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+client.commands = new Collection();
+
+for(const file of commandFiles){
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+}
+
 
 // When the client is ready, run this code (only once)
 client.on('ready', () => {
@@ -14,11 +30,16 @@ client.on('interactionCreate', async interaction => {
 
     if (!interaction.isChatInputCommand()) return;
 
-    const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 
 });
 
