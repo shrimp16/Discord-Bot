94828d287f746bed0ab84fee2b3112752720d953
Shrimp <lcamara0916@gmail.com>
2022-08-07 11:11:04 +0000

    Implemented ping command on the bot to check if evreything is working

--- CasinoBot/main.js
+++ CasinoBot/main.js
@@ -1,13 +1,47 @@
 // Require the necessary discord.js classes
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+const commandsPath = path.join(__dirname, 'commands');
+
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+client.commands = new Collection();
+
+for (const file of commandFiles) {
+
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+
+}
+
 // When the client is ready, run this code (only once)
 client.on('ready', () => {
     console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'Something went wrong!', ephemeral: true });
+    }
+
 });
 
 // Login to Discord with your client's token
