6cfe82378c12134080ad176c0d43deff2f7c91f6
Shrimp <lcamara0916@gmail.com>
2022-08-07 11:48:11 +0000

    Implemented a command for users to claim a daily bonus

--- CasinoBot/commands/create-account.js
+++ CasinoBot/commands/create-account.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const idGenerator = require('../utils/id-generator');
 
-const database = require('../database/db');
-
 const Account = require('../database/tables/accounts');
 
 module.exports = {
@@ -10,14 +8,16 @@ module.exports = {
 		.setName('create-account')
 		.setDescription('Creates a fresh account!'),
 	async execute(interaction) {
+
         const id = idGenerator.getUniqueID();
 
         await Account.create({
             id: id,
             cash: 0
         })
-        
+
         interaction.reply('Create new account!');
         return interaction.user.send(`Here's your new fresh account: ${id}`);
+
 	},
 };
---
+++ CasinoBot/commands/daily-bonus.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const Account = require('../database/tables/accounts');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('daily-bonus')
+        .setDescription('Collect your daily bonus!')
+        .addStringOption(option =>
+            option.setName('account')
+                .setDescription('Insert the account where you want to recieve the bonus.')
+                .setRequired(true)),
+    async execute(interaction) {
+
+        let user = await Account.findOne(
+            { where: { id: interaction.options.getString('account') }}
+        )
+
+        let currentCash = user.dataValues.cash;
+
+        currentCash += 50;
+
+        await Account.update(
+            { cash: currentCash },
+            { where: { id: interaction.options.getString('account') } }
+        )
+
+        return interaction.reply(`Claimed daily bonus! Current balance: ${currentCash}$`);
+
+    },
+};
