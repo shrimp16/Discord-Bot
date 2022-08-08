31fd236887b3ebdd61205d7df81feb82277d76cb
Shrimp <lcamara0916@gmail.com>
2022-08-08 12:42:38 +0000

    Implemented command to check the balance of an account

---
+++ CasinoBot/commands/balance.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const Account = require('../database/tables/accounts');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('balance')
+		.setDescription('Check the balance of an account!')
+        .addStringOption(option =>
+            option.setName('account')
+                .setDescription('Insert the account where you want to check the balance.')
+                .setRequired(true)),
+	async execute(interaction) {
+        let account = await Account.findOne(
+            { where: { id: interaction.options.getString('account') } }
+        )
+		return interaction.reply(`Balance: ${account.dataValues.cash}$`);
+	},
+};
