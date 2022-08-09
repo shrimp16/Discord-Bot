9face27414970bfa5efa4112ce35d8fc043ae5c3
Shrimp <lcamara0916@gmail.com>
2022-08-09 14:13:33 +0000

    Started working on a game for the casino, magic-wheel

---
+++ CasinoBot/commands/magic-wheel.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('magic-wheel')
+		.setDescription('Spin the wheel and try to win!')
+        .addStringOption(option =>
+            option.setName('color')
+                .setDescription('The color you want to bet on!')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'red', value: 'red' },
+                    { name: 'green', value: 'green' },
+                    { name: 'blue', value: 'blue' },
+                )),
+	async execute(interaction) {
+		return interaction.reply(`Roullette! (You picked ${interaction.options.getString('color')})`);
+	},
+};
