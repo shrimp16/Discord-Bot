490e2745b6d5b391142bacc5f0e18178786bf0aa
Shrimp <lcamara0916@gmail.com>
2022-08-11 19:53:52 +0000

    Applied the changes to the daily bonus command

--- CasinoBot/commands/balance.js
+++ CasinoBot/commands/balance.js
@@ -6,14 +6,14 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('balance')
 		.setDescription('Check the balance of an account!')
-        .addStringOption(option =>
+        /*.addStringOption(option =>
             option.setName('account')
                 .setDescription('Insert the account where you want to check the balance.')
-                .setRequired(true)),
+                .setRequired(true))*/,
 	async execute(interaction) {
 
         let account = await Account.findOne(
-            { where: { id: interaction.options.getString('account') } }
+            { where: { id: interaction.user.id } }
         )
 
         if(!account){
