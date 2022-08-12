684f2bfd0a34ede8f69929ca66605a50b256d19d
Shrimp <lcamara0916@gmail.com>
2022-08-12 20:31:32 +0000

    Added the check if the user you want to steal is playing the game

--- CasinoBot/commands/steal.js
+++ CasinoBot/commands/steal.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const Account = require('../database/tables/accounts');
+const fixTag = require('../utils/fix-tag');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('steal')
@@ -9,6 +12,17 @@ module.exports = {
                 .setDescription('The victim of your robbery!')
                 .setRequired(true)),
 	async execute(interaction) {
+
+        let victim_id = fixTag(interaction.options.getString('victim'));
+
+        let victim = Account.findOne(
+            { where: { id: victim_id } }
+        )
+
+        if(!victim){
+            interaction.reply(`The person you tried to steal isn't playing the game!`)
+        }
+
 		return interaction.reply('Pong!');
 	},
 };
--- CasinoBot/commands/transfer.js
+++ CasinoBot/commands/transfer.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Account = require('../database/tables/accounts');
+const fixTag = require('../utils/fix-tag');
 
 module.exports = {
     data: new SlashCommandBuilder()
