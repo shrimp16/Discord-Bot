2e03797c957e952c29102242ef47025506b924a3
Shrimp <lcamara0916@gmail.com>
2022-08-13 15:51:49 +0000

    Implemented the payments on the steal command

--- CasinoBot/commands/steal.js
+++ CasinoBot/commands/steal.js
@@ -13,13 +13,9 @@ module.exports = {
                 .setRequired(true)),
 	async execute(interaction) {
 
-        console.log(interaction.user.id);
-
         let stealer = await Account.findOne(
             { where: { id: interaction.user.id } }
         )
-
-        console.log(interaction.options);
 
         let victim_id = fixTag(interaction.options.getString('victim'));
 
@@ -34,13 +30,38 @@ module.exports = {
 
         let stealResult = Math.floor(Math.random() * 100);
 
+        let newStealerCash;
+        let newVictimCash;
+
         if(stealResult <= 50){
+
             let stealCash = Math.floor(Math.random() * stealer.dataValues.cash);
+
+            newStealerCash = stealer.dataValues.cash - stealCash;
+            newVictimCash = victim.dataValues.cash + stealCash;
+
             interaction.reply(`${interaction.options.getString('victim')} outplayed you and stole ${stealCash} $ from you instead!`);
+
         } else {
+
             let stealCash = Math.floor(Math.random() * victim.dataValues.cash);
+
+            newStealerCash = stealer.dataValues.cash + stealCash;
+            newVictimCash = victim.dataValues.cash - stealCash;
+
             interaction.reply(`You stole ${stealCash} $ from ${interaction.options.getString('victim')}`);
+            
         }
+
+        await Account.update(
+            { cash:  newStealerCash },
+            { where: { id: interaction.user.id }}
+        )
+
+        await Account.update(
+            { cash: newVictimCash },
+            { where: { id: fixTag(interaction.options.getString('victim')) } }
+        )
 
 	},
 };
