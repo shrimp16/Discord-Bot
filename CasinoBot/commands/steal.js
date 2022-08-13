2a48cdf3e084d76f994bbd79b8776126de4c6d1d
Shrimp <lcamara0916@gmail.com>
2022-08-13 15:46:04 +0000

    Implemented the logic to steal from another player

--- CasinoBot/commands/steal.js
+++ CasinoBot/commands/steal.js
@@ -13,16 +13,34 @@ module.exports = {
                 .setRequired(true)),
 	async execute(interaction) {
 
+        console.log(interaction.user.id);
+
+        let stealer = await Account.findOne(
+            { where: { id: interaction.user.id } }
+        )
+
+        console.log(interaction.options);
+
         let victim_id = fixTag(interaction.options.getString('victim'));
 
-        let victim = Account.findOne(
+        let victim = await Account.findOne(
             { where: { id: victim_id } }
         )
 
         if(!victim){
-            interaction.reply(`The person you tried to steal isn't playing the game!`)
+            interaction.reply(`The person you tried to steal isn't playing the game!`);
+            return;
         }
 
-		return interaction.reply('Pong!');
+        let stealResult = Math.floor(Math.random() * 100);
+
+        if(stealResult <= 50){
+            let stealCash = Math.floor(Math.random() * stealer.dataValues.cash);
+            interaction.reply(`${interaction.options.getString('victim')} outplayed you and stole ${stealCash} $ from you instead!`);
+        } else {
+            let stealCash = Math.floor(Math.random() * victim.dataValues.cash);
+            interaction.reply(`You stole ${stealCash} $ from ${interaction.options.getString('victim')}`);
+        }
+
 	},
 };
