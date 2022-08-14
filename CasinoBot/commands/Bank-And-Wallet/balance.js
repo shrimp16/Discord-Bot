27c4d8c120d78a6fd574e16d03602e3b99394776
Shrimp <lcamara0916@gmail.com>
2022-08-14 10:00:01 +0000

    Added options to the balance command for a future update

--- CasinoBot/commands/Bank-And-Wallet/balance.js
+++ CasinoBot/commands/Bank-And-Wallet/balance.js
@@ -3,20 +3,28 @@ const { SlashCommandBuilder } = require('discord.js');
 const Account = require('../../database/tables/accounts');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('balance')
-		.setDescription('Check the balance of an account!'),
+    data: new SlashCommandBuilder()
+        .setName('balance')
+        .setDescription('Check the balance of an account!')
+        .addStringOption(option =>
+            option.setName('color')
+                .setDescription('The color you want to bet on!')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Wallet 👛', value: 'wallet' },
+                    { name: 'Bank 🏦', value: 'bank' }
+                )),
 	async execute(interaction) {
 
         let account = await Account.findOne(
             { where: { id: interaction.user.id } }
         )
 
-        if(!account){
+        if (!account) {
             interaction.reply('Invalid Account!');
             return;
         }
 
-		return interaction.reply(`Balance: ${account.dataValues.cash} $`);
-	},
+        return interaction.reply(`Balance: ${account.dataValues.cash} $`);
+    },
 };
