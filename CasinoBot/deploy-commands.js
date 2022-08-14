162597aa5908520f6118d3f16bf003e20b699286
Shrimp <lcamara0916@gmail.com>
2022-08-14 09:36:27 +0000

    Implemented a more functional aproach to load the commands, added some comments as notes

--- CasinoBot/deploy-commands.js
+++ CasinoBot/deploy-commands.js
@@ -4,9 +4,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [];
-const commandFiles = [];
-
+//ARRAY WITH ALL THE PATHS FOR THE FILES//
 const commandsDirectories = [
 	path.join(__dirname, 'commands'),
 	path.join(__dirname, 'commands/Bank-And-Wallet'),
@@ -15,11 +13,18 @@ const commandsDirectories = [
 	path.join(__dirname, 'commands/Risky')
 ];
 
-
+//CREATES AN ARRAY OF ARRAYS WITH ALL THE FILES PATH FROM EACH DIRECTORY//
+const commandFiles = [];
 for(const directory of commandsDirectories){
 	commandFiles.push(getFiles(directory));
 }
 
+/*
+IT IS THE ALMOST THE SAME AS THE ONE ON THE MAIN FILE BUT THIS ONE ADDS THE COMMANDS DATA
+TO AN ARRAY OF COMMANDS AS JSON TO THEN DEPLOY
+*/
+const commands = [];
+//FIRST TRY TO GET THIS LOADER TO WORK//
 for(let i = 0; i < commandFiles.length; i++){
 	for(let b = 0; b < commandFiles[i].length; b++){
 		const filePath = path.join(commandsDirectories[i] ,commandFiles[i][b]);
@@ -27,8 +32,6 @@ for(let i = 0; i < commandFiles.length; i++){
 		commands.push(command.data.toJSON());
 	}
 }
-
-console.log(commands);
 
 function getFiles(dir){
 
--- CasinoBot/main.js
+++ CasinoBot/main.js
@@ -1,5 +1,5 @@
 // Require the necessary discord.js classes
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, ClientUser } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
@@ -9,6 +9,7 @@ const database = require('./database/db');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+//ARRAY WITH ALL THE PATHS FOR THE FILES//
 const commandsDirectories = [
 	path.join(__dirname, 'commands'),
 	path.join(__dirname, 'commands/Bank-And-Wallet'),
@@ -17,26 +18,45 @@ const commandsDirectories = [
 	path.join(__dirname, 'commands/Risky')
 ];
 
-const commandFiles = [];
+//INSTEAD OF HAVING AN ARRAY, A SIMPLE VARIABLE IS ENOUGH IN CASE THERE'S ONLY ONE DIRECTORY FOR ALL THE COMMANDS//
+//const commandsPath = path.join(__dirname, 'commands');
 
+//CREATES AN ARRAY OF ARRAYS WITH ALL THE FILES PATH FROM EACH DIRECTORY//
+const commandFiles = [];
 for(const directory of commandsDirectories){
 	commandFiles.push(fs.readdirSync(directory).filter(file => file.endsWith('.js')));
 }
 
-console.log(commandFiles);
-
+//THIS IS THE WAY TO CREATE THE ARRAY WITH ALL THE FILES PATH WITH ONLY ONE DIRECTORY//
 //const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+/*
+IT IS THE ALMOST THE SAME AS THE ONE ON THE DEPLOY COMMANDS FILE BUT THIS ONE ADDS THE COMMANDS DATA//
+TO A COLLECTION FOR THE USERS TO INVOKE
+*/
 client.commands = new Collection();
-
-for(let i = 0; i < commandFiles.length; i++){
+//FIRST TRY TO GET THIS LOADER TO WORK//
+/*for(let i = 0; i < commandFiles.length; i++){
 	for(let b = 0; b < commandFiles[i].length; b++){
-		const filePath = path.join(commandsDirectories[i] ,commandFiles[i][b]);
+		const filePath = path.join(commandsDirectories[i], commandFiles[i][b]);
 		const command = require(filePath);
 		client.commands.set(command.data.name, command);
 	}
-}
+}*/
 
+//SOME EXPERIMENTS WITH FUNCTIONAL PROGRAMMING TO LOAD THE COMMANDS//
+let pointer = 0;
+
+commandFiles.forEach(commands => {
+    commands.forEach(command => {
+        const filePath = path.join(commandsDirectories[pointer], command);
+        const commandCode = require(filePath);
+        client.commands.set(commandCode.data.name, commandCode);
+    });
+    pointer++;
+})
+
+//OLD WAY THAT WORKS WHEN ALL THE COMMAND FILES ARE ON THE SAME DIRECTORY//
 /*for (const file of commandFiles) {
 
     const filePath = path.join(commandsPath, file);
@@ -45,8 +65,6 @@ for(let i = 0; i < commandFiles.length; i++){
     client.commands.set(command.data.name, command);
 
 }*/
-
-console.log(client.commands);
 
 //keeping this here just in case I need to reset the database
 /*
