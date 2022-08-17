4f65b118740a2f4c103399992069d41637133de7
Shrimp <lcamara0916@gmail.com>
2022-08-17 10:18:13 +0000

    Added a new option to the odd-even command

--- CasinoBot/commands/Casino-Games/odd-even.js
+++ CasinoBot/commands/Casino-Games/odd-even.js
@@ -7,15 +7,27 @@ module.exports = {
         .addStringOption(option =>
             option.setName('odd-even')
                 .setDescription('Is it gonna be odd or even?')
-                .setRequired(true))
+                .setRequired(true)
                 .addChoices(
                     { name: 'Odd', value: 'odd' },
-                    { name: 'Even', value: 'even' })
+                    { name: 'Even', value: 'even' }))
+        .addIntegerOption(option =>
+            option.setName('number')
+                .setDescription('The number you want to add up!')
+                .setRequired(true)
+                .addChoices(
+                    { name: '1', value: 1},
+                    { name: '2', value: 2},
+                    { name: '3', value: 3},
+                    { name: '4', value: 4},
+                    { name: '5', value: 5},
+                    ))
         .addIntegerOption(option =>
             option.setName('bet')
                 .setDescription('The amount of cash you want to bet!')
                 .setRequired(true)),
     async execute(interaction) {
+
         return interaction.reply('Currently implementing this command!');
     },
 };
