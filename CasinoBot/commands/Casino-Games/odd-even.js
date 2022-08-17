6428da8f262f5728169a0875a9a40a2f5137e498
Shrimp <lcamara0916@gmail.com>
2022-08-17 10:35:53 +0000

    Add payments to the odd-even game

--- CasinoBot/commands/Casino-Games/odd-even.js
+++ CasinoBot/commands/Casino-Games/odd-even.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const Account = require('../../database/tables/accounts');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -16,12 +17,12 @@ module.exports = {
                 .setDescription('The number you want to add up!')
                 .setRequired(true)
                 .addChoices(
-                    { name: '1', value: 1},
-                    { name: '2', value: 2},
-                    { name: '3', value: 3},
-                    { name: '4', value: 4},
-                    { name: '5', value: 5},
-                    ))
+                    { name: '1', value: 1 },
+                    { name: '2', value: 2 },
+                    { name: '3', value: 3 },
+                    { name: '4', value: 4 },
+                    { name: '5', value: 5 },
+                ))
         .addIntegerOption(option =>
             option.setName('bet')
                 .setDescription('The amount of cash you want to bet!')
@@ -34,21 +35,52 @@ module.exports = {
 
         const randomNumber = Math.floor(Math.random() * 6);
 
+        const user = await Account.findOne({
+            where: { id: interaction.user.id }
+        })
+
         const OddOrEven = ((randomNumber + userNumber) % 2 === 0);
 
-        if(OddOrEven && userBet === 'even'){
+        if (OddOrEven && userBet === 'even') {
             interaction.reply(`I played ${randomNumber} and it's even! you win!`);
+            win();
             return;
-        } else if(OddOrEven && userBet === 'odd'){
+        } else if (OddOrEven && userBet === 'odd') {
             interaction.reply(`I played ${randomNumber} and it's even! I win!`);
+            lose();
             return;
-        } else if(!OddOrEven && userBet === 'even'){
+        } else if (!OddOrEven && userBet === 'even') {
             interaction.reply(`I played ${randomNumber} and it's odd! I win!`);
+            lose();
             return;
         } else {
-            interaction.reply(`I played ${randomNumber} and it's odd! You win!`)
+            interaction.reply(`I played ${randomNumber} and it's odd! You win!`);
+            win();
             return;
         }
-        
+
+        function win() {
+
+            const newCash = user.dataValues.wallet_cash + betAmount;
+
+            pay(newCash);
+
+        }
+
+        function lose() {
+
+            const newCash = user.dataValues.wallet_cash - betAmount;
+
+            pay(newCash);
+
+        }
+
+        async function pay(cash) {
+            await Account.update(
+                { wallet_cash: cash },
+                { where: { id: interaction.user.id } }
+            )
+        }
+
     },
 };
