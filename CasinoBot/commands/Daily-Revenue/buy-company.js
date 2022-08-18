0d25c6f81e53bdeac9c786151a40f8bce9a4a27c
Shrimp <lcamara0916@gmail.com>
2022-08-18 10:04:56 +0000

    Created a new command with options for users to buy companies

--- CasinoBot/commands/Daily-Revenue/buy-company.js
+++ CasinoBot/commands/Daily-Revenue/buy-company.js
@@ -20,3 +20,24 @@ const Companies = [
         price: 100000
     }
 ]
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('buy-company')
+		.setDescription('Buy a company to get passive income!')
+        .addStringOption(option => 
+            option.setName('company')
+            .setDescription('The company you want to buy!')
+            .setRequired(true)
+            .addChoices(
+                { name: 'Dollar Store', value: 'Dollar Store' },
+                { name: 'Coffee Shop', value: 'Coffee Shop' },
+                { name: 'Bakery', value: 'Bakery' },
+                { name: 'Supermarket', value: 'Supermarket' }
+            )),
+	async execute(interaction) {
+		return interaction.reply('Still in development!');
+	},
+};
