db73560466740ed3e04b86bd739e382b3d3e7f82
Shrimp <lcamara0916@gmail.com>
2022-08-18 09:53:58 +0000

    Created an object with the first batch of companies

---
+++ CasinoBot/commands/Daily-Revenue/buy-company.js
@@ -0,0 +1,22 @@
+const Companies = [
+    {
+        name: "Dollar Store",
+        production: 20,
+        price: 5000
+    },
+    {
+        name: "Coffee Shop",
+        production: 50,
+        price: 15000
+    },
+    {
+        name: "Bakery",
+        production: 150,
+        price: 30000
+    },
+    {
+        name: "Supermarket",
+        production: 300,
+        price: 100000
+    }
+]
